b8b6c88a5cbdf867b43092edfae7dffd62918990
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2024-12-11 21:30:48 +0200

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import AboutPage from './pages/AboutPage/AboutPage'
-import ArticlesPage from './pages/ArticlesPage/ArticlesPage'
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+// import HomePage from './pages/HomePage/HomePage'
+// import AboutPage from './pages/AboutPage/AboutPage'
+// import ArticlesPage from './pages/ArticlesPage/ArticlesPage'
+// import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
 
-import Navigation from './components/Navigation/Navigation'
-import ArticlesDetails from './pages/ArticlesDetails/ArticlesDetails'
+// import Navigation from './components/Navigation/Navigation'
+// import ArticlesDetails from './pages/ArticlesDetails/ArticlesDetails'
+import { lazy, Suspense } from 'react'
+// import Some from './components/Some/Some'
+
+const Some = lazy(() => import('./components/Some/Some'))
+const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'))
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'))
+const ArticlesPage = lazy(() => import('./pages/ArticlesPage/ArticlesPage'))
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'))
+const Navigation = lazy(() => import('./components/Navigation/Navigation'))
+const ArticlesDetails = lazy(() => import('./pages/ArticlesDetails/ArticlesDetails'))
+
 const App = () => {
   //
   return (
     <div>
-      <h1>Header</h1>
+      {/* <h1>Header</h1> */}
       <Navigation />
-
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/articles' element={<ArticlesPage />} />
-        <Route path='/articles/:articleId' element={<ArticlesDetails />}>
-          <Route path='somePath' element={<h2>Some Path</h2>} />
-          <Route path='somePath2' element={<h2>Some Path 2</h2>} />
-          <Route path='somePath3' element={<h2>Some Path 3</h2>} />
-        </Route>
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<h1>LOADING!!!!!!!!!!!!!!!!!!</h1>}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/articles' element={<ArticlesPage />} />
+          <Route path='/articles/:articleId' element={<ArticlesDetails />}>
+            <Route path='somePath' element={<Some />} />
+            <Route path='somePath2' element={<Some />} />
+            <Route path='somePath3' element={<Some />} />
+          </Route>
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
       <footer>footer</footer>
     </div>
   )
